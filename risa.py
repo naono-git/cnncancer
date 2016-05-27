b31d202b543de099b2d484e2b625defcf9536c05
Naoaki ONO <nono@is.naist.jp>
2016-05-27 12:17:27 -0700

    risa noconv

--- risa.py
+++ risa.py
@@ -27,30 +27,33 @@ ss = 32 # sample size
 if(not 'qqq_trn' in locals()):
     file_input = 'qqq_trn_w{}.npy'.format(ss)
     path_data = os.path.join(dir_input,'input_w{}'.format(ss),file_input)
-    qqq_trn = np.load(path_data)
+    tmp = np.load(path_data)
+    tmp1 = []
+    for aa in range(4):
+        for bb in range(4):
+            tmp1.append(tmp[:,(0+aa*8):(8+aa*8),(0+bb*8):(8+bb*8),:])
+    qqq_trn = np.vstack(tmp1)
     print('load input from {}'.format(path_data))
 
 nn,ny,nx,nl = qqq_trn.shape
 print('nn ny nx nl',nn,ny,nx,nl)
 
 nf_RGB = 3
-nf_conv1 = 12
+nf_risa = 24
 
-ww = tf.Variable(tf.truncated_normal([fs_1,fs_1,nf_RGB,12],stddev=0.05))
+fs_1 = 8
+ww = tf.Variable(tf.truncated_normal([fs_1,fs_1,nf_RGB,nf_risa],stddev=0.05))
 tf_input = tf.placeholder(tf.float32, [None,ny,nx,nl])
 
-tf_conv1 = tf.nn.conv2d(tf_input,ww,strides=[1,1,1,1],padding='SAME')
-tf_deconv1 = tf.nn.conv2d_transpose(value=tf_conv1,filter=ww,
-                                    output_shape=[batch_size,ny,nx,nl],
-                                    strides=[1,1,1,1],
-                                    padding='SAME')
+tf_conv1 = tf.nn.conv2d(tf_input,ww,strides=[1,1,1,1],padding='VALID')
+tf_deconv1 = tf.nn.conv2d_transpose(value=tf_conv1,filter=ww,output_shape=[batch_size,ny,nx,nl],strides=[1,1,1,1],padding='VALID')
 ## tf_error = tf.reduce_mean(tf.square(tf_deconv1 - tf_input))
 tf_error = tf.nn.l2_loss(tf_deconv1 - tf_input)
 
 tf_simple1 = tf.square(tf_conv1)
-seg12 = tf.constant([1,1,2,2,3,3,4,4,5,5,6,6])
+seg24 = tf.constant([0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11])
 tf_t_simple1 = tf.transpose(tf_simple1)
-tf_sparce1 = tf.reduce_mean(tf.sqrt(tf.segment_sum(tf_t_simple1,vv)))
+tf_sparce1 = tf.reduce_mean(tf.sqrt(tf.segment_sum(tf_t_simple1,seg24)))
 
 tf_cost = tf_error + lambda_s * tf_sparce1
 # tf_cost = tf_error + lambda_s * tf_sparce + lambda_n * tf_norm
@@ -66,7 +69,7 @@ iii_batches = np.split(iii_nn,iii_bin)
 
 for tt in range(tmax):
     if(tt % tprint==0):
-        tmp = [sess.run(mean_error,{tf_input: qqq_trn[iii,]}) for iii in iii_batches]
+        tmp = [tf_error.eval({tf_input: qqq_trn[iii,]}) for iii in iii_batches]
         error_out = np.mean(tmp)
         print(tt,error_out)
     np.random.shuffle(iii_nn)
