af8a0f017db2e35c137571293ae977dfceec52d0
Naoaki ONO <nono@is.naist.jp>
2017-03-05 08:27:21 +0900

    predict3

--- pancreas_layer_predict3_ki.py
+++ pancreas_layer_predict3_ki.py
@@ -2,60 +2,58 @@
 # -*- coding: utf-8 -*-
 # <nbformat>3.0</nbformat>
 
-print('setup stacked pancreas predictor stage2')
+print('setup stacked pancreas predict3 ki')
 exec(open('extern_params.py').read())
 
 #
 # setup parameters
 #
 
-# extern stamp2
-if(stamp2=='NA'):
-    print('initialize w2 and b2 randomly')
-    weight2 = {
-        'encode': tf.Variable(tf.truncated_normal([fs_2,fs_2,nf_conv2,nf_encode2],stddev=0.05)),
-        'decode': tf.Variable(tf.truncated_normal([fs_2,fs_2,nf_encode2,nf_RGB],stddev=0.05)),
+# extern stamp3
+if(stamp3=='NA'):
+    print('initialize w3 and b3 randomly')
+    weight3 = {
+        'encode': tf.Variable(tf.truncated_normal([fs_3,fs_3,nf_encode2,nf_encode3],stddev=0.05)),
+        'decode': tf.Variable(tf.truncated_normal([fs_3,fs_3,nf_encode3,nf_RGB],stddev=0.05)),
     }
-    bias2 = {
-        'encode': tf.Variable(tf.zeros([nf_encode2])),
+    bias3 = {
+        'encode': tf.Variable(tf.zeros([nf_encode3])),
         'decode': tf.Variable(tf.zeros([nf_RGB])),
     }
 else:
-    print('load w2 and b2 from',stamp2)
-    file_w2 = 'weight2.{}.pkl'.format(stamp2)
-    file_b2 = 'bias2.{}.pkl'.format(stamp2)
+    print('load w3 and b3 from',stamp3)
+    file_w3 = 'weight3.{}.pkl'.format(stamp3)
+    file_b3 = 'bias3.{}.pkl'.format(stamp3)
 
-    path_w2 = os.path.join('out1',file_w2)
-    path_b2 = os.path.join('out1',file_b2)
+    path_w3 = os.path.join(dir_out,file_w3)
+    path_b3 = os.path.join(dir_out,file_b3)
 
-    if(not os.path.exists(path_w2) or not os.path.exists(path_b2)):
-        myutil.getRemoteFile([file_w2,file_b2],dirname='Documents/cnncancer/out1')
+    if(not os.path.exists(path_w3) or not os.path.exists(path_b3)):
+        myutil.getRemoteFile([file_w3,file_b3],dirname=os.path.join(dir_remote,dir_out))
 
-    weight2 = tensorflow_ae_base.load_tf_variable(path_w2,key2,trainable=trainable2)
-    bias2   = tensorflow_ae_base.load_tf_variable(path_b2,key2,trainable=trainable2)
+    weight3 = tensorflow_ae_base.load_tf_variable(path_w3,key3,trainable=trainable3)
+    bias3   = tensorflow_ae_base.load_tf_variable(path_b3,key3,trainable=trainable3)
 #end if(stamp=='NA')
 
 #
 # setup layers
 #
 
-def get_predict2(qqq):
-    encode2 = tf.nn.relu(conv2d(qqq,  weight2['encode'],bias2['encode']))
-    predict2 = tf.nn.relu(conv2d(encode2,  weight2['decode'],bias2['decode']))
-    return(predict2)
+def get_predict3(qqq):
+    encode3 = tf.nn.relu(conv2d(qqq,  weight3['encode'],bias3['encode']))
+    predict3 = tf.nn.relu(conv2d(encode3,  weight3['decode'],bias3['decode']))
+    return(predict3)
 
-def get_dist2(qqq):
+def get_dist3(qqq):
     pool2   = max_pool(qqq,kk=pool_size)
-    return(pool2)
+    pool3   = max_pool(pool2,kk=pool_size)
+    return(pool3)
 #
 # save network parameters
 #
-def save_stage2():
-    weight2_fin = {k:sess.run(v) for k,v in weight2.items()}
-    bias2_fin = {k:sess.run(v) for k,v, in bias2.items()}
-    myutil.saveObject(weight2_fin,'weight2.{}.pkl'.format(stamp))
-    myutil.saveObject(bias2_fin,'bias2.{}.pkl'.format(stamp))
-    return([weight2_fin,bias2_fin])
-
-
-
+def save_stage3():
+    weight3_fin = {k:sess.run(v) for k,v in weight3.items()}
+    bias3_fin = {k:sess.run(v) for k,v, in bias3.items()}
+    myutil.saveObject(weight3_fin,'weight3.{}.pkl'.format(stamp))
+    myutil.saveObject(bias3_fin,'bias3.{}.pkl'.format(stamp))
+    return([weight3_fin,bias3_fin])
--- pancreas_train_predict3_ki.py
+++ pancreas_train_predict3_ki.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # <nbformat>3.0</nbformat>
 
-print('train stage2 predictor of ki57')
+print('train stage3 predictor of ki57')
 
 import os
 import sys
@@ -23,52 +23,40 @@ from myutil import source
 
 exec(open('extern_params.py').read())
 
+ss = 32
 #
 # load sample data
 #
-dir_data = 'dat1'
-if(not ('qqq_src' in locals() or 'qqq_dst' in locals())):
-    file_src = 'datafile_w1024_he_1.npy'
-    path_src = os.path.join(dir_data,file_src)
-    data_src = np.load(path_src)
-    print('load input from {}'.format(path_src))
+file_src = 'pancreas_he_w{}.npy'.format(ss)
+path_src = os.path.join(dir_data,file_src)
+print('load input from {}'.format(path_src))
+qqq_src = np.load(path_src)
 
-    file_dst = 'datafile_w1024_ki_1.npy'
-    path_dst = os.path.join(dir_data,file_dst)
-    data_dst = np.load(path_dst)
-    print('load input from {}'.format(path_dst))
-
-np.random.seed(random_seed)
-
-ns = 10000
-qqq_src = np.zeros((ns,32,32,3))
-qqq_dst = np.zeros((ns,32,32,3))
-for aa in range(ns):
-    i0 = np.random.choice(range(1024-32-100),size=1)[0]+100
-    j0 = np.random.choice(range(1024-32-100),size=1)[0]+100
-    i1 = i0+32
-    j1 = j0+32
-    qqq_src[aa,:]=data_src[j0:j1,i0:i1,:]
-    qqq_dst[aa,:]=data_dst[j0:j1,i0:i1,:]
+file_dst = 'pancreas_ki_w{}.npy'.format(ss)
+path_dst = os.path.join(dir_data,file_dst)
+print('load input from {}'.format(path_dst))
+qqq_dst = np.load(path_dst)
 
 nn,ny,nx,nl = qqq_src.shape
 print('nn ny nx nl',nn,ny,nx,nl)
-trainable2=True
-trainable1=False
 exec(open('tensorflow_ae_stage1.py').read())
-exec(open('pancreas_ki2.py').read())
+exec(open('tensorflow_ae_stage1.py').read())
+exec(open('pancreas_layer_predict3_ki.py').read())
 
 #
 # setup optimizer
 #
 tf_src = tf.placeholder(tf.float32, [None,ny,nx,nl])
 tf_dst = tf.placeholder(tf.float32, [None,ny,nx,nl])
-tf_dst2 = get_dist2(tf_dst)
+tf_dst3 = get_dist3(tf_dst)
 tf_encode1 = get_encode1(tf_src)
-tf_predict = get_predict2(tf_encode1)
-mean_error = tf.reduce_mean(tf.square(tf_predict - tf_dst2))
+tf_encode2 = get_encode2(tf_encode1)
+tf_predict = get_predict3(tf_encode2)
+mean_error = tf.reduce_mean(tf.square(tf_predict - tf_dst3))
 optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate)
 train = optimizer.minimize(mean_error)
+
+sess.run(tf.initialize_all_variables())
 
 #
 # train loop
@@ -77,12 +65,11 @@ iii_bin = np.arange(batch_size,nn,batch_size)
 iii_nn = np.arange(nn)
 iii_batches = np.split(iii_nn,iii_bin)
 
-sess.run(tf.initialize_all_variables())
-
 for tt in range(tmax):
     if(tt % tprint==0):
-        error_out = np.mean([sess.run(mean_error,{tf_src: qqq_src[iii,],tf_dst:qqq_dst[iii,]}) for iii in iii_batches])
-        print(tt,error_out)
+        error_out = [mean_error.eval({tf_src: qqq_src[iii],tf_dst:qqq_dst[iii]})
+                     for iii in iii_batches]
+        print(tt,np.mean(error_out))
     np.random.shuffle(iii_nn)
     iii_batches = np.split(iii_nn,iii_bin)
     for iii in iii_batches:
@@ -91,7 +78,11 @@ for tt in range(tmax):
 #
 # save parameters
 #
-save_stage1()
-save_stage2()
-myutil.timestamp()
-print('stamp2 = \'{}\''.format(stamp))
+if(trainable1):
+    save_stage1()
+if(trainable2):
+    save_stage2()
+save_stage3()
+
+myutil.show_timestamp()
+print('stamp3 = \'{}\''.format(stamp))
