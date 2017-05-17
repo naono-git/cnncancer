5993117313801b75b7558a262ed5dd9b5e52cf72
Naoaki ONO <nono@is.naist.jp>
2017-05-17 18:47:46 +0900

    new

---
+++ cnncancer_sonnet.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+print("cnncancer_sonnet")
+# <codecell>
+
+import numpy as np
+import tensorflow as tf
+import sonnet as snt
+
+# define modules
+# define network
+# resotre or initialize model
+# read training and test data
+# save
+# tensorboard
+# train
+# verify
